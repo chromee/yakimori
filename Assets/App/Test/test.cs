8c8706e5f6c505131c45927121188fadc3d746ad
chromee <odk.mmmm@gmail.com>
2018-11-25 17:39:22 +0900

    change

---
+++ Assets/App/Test/test.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+
+    public float v;
+    void Update()
+    {
+        transform.Rotate(Vector3.up, v);
+    }
+}
